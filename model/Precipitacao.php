647e8ed9898b0da28438876eb9a9d7693fa4d1cb
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-05-02 10:52:47 -0300

    Finished Precipitacao.php and model structure

---
+++ model/Precipitacao.php
@@ -0,0 +1,45 @@
+<?php
+class Precipitacao {
+	private $prefixo, $ano, $mes, $media;
+
+	public __constructor($prefixo, $ano, $mes, $media) {
+		$this->prefixo = $prefixo;
+		$this->ano = $ano;
+		$this->mes = $mes;
+		$this->media = $media;
+	}
+
+	function getPrefixo() {
+		return $this->prefixo;
+	}
+
+	function setPrefixo($prefixo) {
+		$this->prefixo = $prefixo;
+	}
+
+	function getAno() {
+		return $this->ano;
+	}
+
+	function setAno($ano) {
+		$this->ano = $ano;
+	}
+
+	function getMes() {
+		return $this->mes;
+	}
+
+	function setMes($mes) {
+		$this->mes = $mes;
+	}
+
+	function getMedia() {
+		return $this->media;
+	}
+
+	function setMedia($media) {
+		$this->media = $media;
+	}
+}
+
+?>
