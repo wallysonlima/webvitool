9c8ce701087c455bf8257660b32b67689c87327c
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-05-03 10:44:17 -0300

    Finished PrecipitacaoDAO class from dao

--- dao/PostoDAO.php
+++ dao/PostoDAO.php
@@ -139,13 +139,13 @@ class PostoDAO {
 
 	public function getInfoPosto() {
 		conectar();
-		$sql = $this->con->prepare("SELECT prefixo, municipio, bacia, latitude, longitude FROM `posto` WHERE prefixo IN (" +
-                "'D4-004', 'B7-047', " +
-                "'C5-008', 'D7-020', 'E3-074', 'D5-080', 'B5-002', 'D6-001', 'D5-019', " +
-                "'D3-002', 'E5-047', 'E3-038', 'C8-043', 'B4-001', 'E3-002', 'E4-135', 'E4-023', 'C5-025', " +
-                "'B7-008', 'E3-025.dat', 'D4-064', 'D6-010', 'E3-264', 'D4-002', 'D8-003', " +
-                "'F4-004', 'C4-034.dat', 'E3-041', 'C4-019', 'C3-031', 'B6-014', 'D2-021', 'E3-003', 'E2-045', " +
-                "'E4-056', 'E2-022', 'B6-032') " + "GROUP BY prefixo;");
+		$sql = $this->con->prepare("SELECT prefixo, municipio, bacia, latitude, longitude FROM `posto` WHERE prefixo IN (".
+                "'D4-004', 'B7-047', ".
+                "'C5-008', 'D7-020', 'E3-074', 'D5-080', 'B5-002', 'D6-001', 'D5-019', ".
+                "'D3-002', 'E5-047', 'E3-038', 'C8-043', 'B4-001', 'E3-002', 'E4-135', 'E4-023', 'C5-025', ".
+                "'B7-008', 'E3-025.dat', 'D4-064', 'D6-010', 'E3-264', 'D4-002', 'D8-003', ".
+                "'F4-004', 'C4-034.dat', 'E3-041', 'C4-019', 'C3-031', 'B6-014', 'D2-021', 'E3-003', 'E2-045', ".
+                "'E4-056', 'E2-022', 'B6-032') GROUP BY prefixo;");
 
 		$sql->bindValue(1, $prefixo);
 		$sql->execute();
@@ -162,3 +162,5 @@ class PostoDAO {
 		return $postos;
 	}
 }
+
+?>
---
+++ dao/PrecipitacaoDAO.php
@@ -0,0 +1,152 @@
+<?php
+	/* Developed by: Wallyson Lima
+	   Email: wallyson.n.a.lima@gmail.com
+	   Github: Wallyson Lima
+	   Date: 03/05/2018
+	*/
+include_once("/home/wlima/Documents/TCC/codigos/webvitool/Model/Database.php");
+include_once("/home/wlima/Documents/TCC/codigos/webvitool/Model/Precipitacao.php");
+
+class PrecipitacaoDAO {
+	private $con;
+	private $tableName;
+
+	public function conectar() {
+		$db = new Database();
+		$this->con = $db->connection();
+		$this->tableName = "posto";
+	}
+
+	public function desconectar() {
+		$con = null;
+	}
+
+	public function getMediaChuvaMes($prefixo, $ano) {
+		conectar();
+		$sql = $this->con->prepare("SELECT ano, mes, ".
+                "d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,".
+                " d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25, d26, d27,".
+                " d28, d29, d30, d31 FROM `precipitacao` WHERE prefixo='".$prefixo.".dat' and ano >='".$ano."' group by ano, mes");
+
+		$sql->execute();
+		$pre = array();
+
+		while ( $row = $sql->fetch() )
+		{
+			$dias = array();
+			$ano = $row[0];
+			$mes = $row[1];
+
+			for($i = 2; $i < 33; $i++)
+				array_push($dias, (Float) $row[$i]);
+
+			$media = getMedia($dias);
+
+			$obj = new Precipitacao($prefixo, $ano, $mes, $media);
+			array_pus($pre, $obj);
+		}
+
+		desconectar();
+		return $pre;
+	}
+
+	public function getMediaChuvaAno($prefixo, $ano) {
+		conectar();
+
+		$sql = $this->con->prepare("SELECT ano, mes, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,".
+                " d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25, d26, d27,".
+                " d28, d29, d30, d31 FROM `precipitacao` WHERE prefixo='".$prefixo.".dat' and".
+                " ano='".$ano."' group by mes;");
+
+		$sql->execute();
+		$pre = array();
+
+		while ( $row = $sql->fetch() )
+		{
+			$dias = array();
+			$ano = $row[0];
+			$mes = $row[1];
+
+			for($i = 2; $i < 33; $i++)
+				array_push($dias, (Float) $row[$i]);
+
+			$media = getMedia($dias);
+
+			$obj = new Precipitacao($prefixo, $ano, $mes, $media);
+			array_pus($pre, $obj);
+		}
+
+		desconectar();
+		return $pre;
+	}
+
+	public function getMedia($dias) {
+		$media = 0;
+		$count = 0;
+
+		foreach($dias as &$d) {
+			if ( $d != 9999 ) {
+				$media += $d;
+				$count++;
+			}
+		}
+
+		if ( count == 0 )
+			return 0;
+
+		return number_format((float)$media/(++$count), 2, '.', '');
+	}
+
+	public function getMediaChuvaMesPostos($ano, $mes) {
+		conectar();
+		$sql = $this->con->prepare("SELECT ".
+                "d1, d2, d3, d4, d5, d6, d7, d8, d9, d10,".
+                " d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25, d26, d27,".
+                " d28, d29, d30, d31 FROM `precipitacao` WHERE prefixo IN (".
+                "'D4-004.dat', 'B7-047.dat', ".
+                "'C5-008.dat', 'D7-020.dat', 'E3-074.dat', 'D5-080.dat', 'B5-002.dat', 'D6-001.dat', 'D5-019.dat', ".
+                "'D3-002.dat', 'E5-047.dat', 'E3-038.dat', 'C8-043.dat', 'B4-001.dat', 'E3-002.dat', 'E4-135.dat', 'E4-023.dat', 'C5-025.dat', "."'B7-008.dat', 'E3-025.dat', 'D4-064.dat', 'D6-010.dat', 'E3-264.dat', 'D4-002.dat', 'D8-003.dat', ".
+                "'F4-004.dat', 'C4-034.dat', 'E3-041.dat', 'C4-019.dat', 'C3-031.dat', 'B6-014.dat', 'D2-021.dat', 'E3-003.dat', 'E2-045.dat', ".
+                "'E4-056.dat', 'E2-022.dat', 'B6-032.dat') ".
+                "and ano=? and mes=?;");
+
+		$sql->bindValue(1, $ano);
+		$sql->bindValue(2, $mes);
+		$sql->execute();
+		$pre = array();
+
+		while ( $row = $sql->fetch() )
+		{
+			$dias = array();
+
+			for($i = 0; $i < 31; $i++)
+				array_push($dias, (Float) $row[$i]);
+
+			$media = getMedia($dias);
+			array_push($pre, $media);
+		}
+
+		desconectar();
+		return $pre;
+	}
+
+	public function getQtdeMes($prefixo, $ano) {
+		conectar();
+		$sql = $this->con->prepare("SELECT COUNT(mes) as qtde FROM `precipitacao` WHERE prefixo='".$prefixo.".dat' and ano='".$ano."';");
+		$mes = 0;
+
+		while ( $row = $sql->fetch() )
+		{
+			$mes = (int) $row;
+			desconectar();
+			return $mes;
+		}
+
+		return null;
+	}
+
+}
+
+	
+
+?>
