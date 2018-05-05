1de556367fc74a446763fd39263284464701e2fd
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-05-05 10:06:52 -0300

    Created zoomChart.html

--- view/zoomChart.php
+++ view/zoomChart.php
@@ -23,10 +23,10 @@ require_once("nav.php");
 echo '
 	<section>
 	  <div id="container">  
-		<form method="post" action="../control/timelineChart.php">
+		<form method="post" action="../control/zoomChart.php">
 	      <div>
-	        <label>Prefixo1</label>
-	        <select name="prefixo1">
+	        <label>Prefixo</label>
+	        <select name="prefixo">
 	          <option value="january">January</option>
 	          <option value="february">February</option>
 	          <option value="may">May</option>
@@ -34,8 +34,8 @@ echo '
 	      </div>
 	      
 	      <div>
-	        <label>Prefixo2</label>
-	        <select name="prefixo2">
+	        <label>Ano</label>
+	        <select name="ano">
 	          <option value="january">January</option>
 	          <option value="february">February</option>
 	          <option value="may">May</option>
@@ -43,35 +43,8 @@ echo '
 	      </div>
 
 	      <div>
-	        <label>Prefixo3</label>
-	        <select name="prefixo3">
-	          <option value="january">January</option>
-	          <option value="february">February</option>
-	          <option value="may">May</option>
-	        </select>
-	      </div>
-
-	      <div>
-	        <label>Ano1</label>
-	        <select name="ano1">
-	          <option value="january">January</option>
-	          <option value="february">February</option>
-	          <option value="may">May</option>
-	        </select>
-	      </div>
-
-	      <div>
-	        <label>Ano2</label>
-	        <select name="ano2">
-	          <option value="january">January</option>
-	          <option value="february">February</option>
-	          <option value="may">May</option>
-	        </select>
-	      </div>
-
-	      <div>
-	        <label>Ano3</label>
-	        <select name="ano3">
+	        <label>Quantidade</label>
+	        <select name="qtde">
 	          <option value="january">January</option>
 	          <option value="february">February</option>
 	          <option value="may">May</option>
